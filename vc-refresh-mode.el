f8700cc73bce9a8b4856fa745cdeb7d72fe31b10
Jen-Chieh Shen <jcs090218@gmail.com>
2022-12-03 23:34:54 +0800

    add src

---
+++ vc-refresh-mode.el
@@ -0,0 +1,37 @@
+;;; vc-refresh-mode.el --- Refresh vc-state in certain events for better UX  -*- lexical-binding: t; -*-
+
+;; Copyright (C) 2022  Shen, Jen-Chieh
+
+;; Author: Shen, Jen-Chieh <jcs090218@gmail.com>
+;; Maintainer: Shen, Jen-Chieh <jcs090218@gmail.com>
+;; URL: https://github.com/jcs090218/vc-refresh-mode
+;; Version: 0.0.1
+;; Package-Requires: ((emacs "24.3"))
+;; Keywords: vc
+
+;; This file is not part of GNU Emacs.
+
+;; This program is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This program is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program. If not, see <https://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;
+;; Refresh vc-state in certain events for better UX
+;;
+
+;;; Code:
+
+
+
+(provide 'vc-refresh-mode)
+;;; vc-refresh-mode.el ends here
